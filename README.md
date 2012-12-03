71f6090beccd2acc4f7504aa72e0e73849657355
sprite728 <chuanche@umich.edu>
2012-12-03 00:44:56 -0500

    Implemented fundamental xyplot

--- README.md
+++ README.md
@@ -2,3 +2,123 @@ infoviz-worldbankdata
 =====================
 
 This is a group project for the Information Visualization course in U Mich, which we are aiming to visualize some of the data from The World Bank
+
+Simple Testing Server
+=====================
+python -m SimpleHTTPServer 3000
+
+Reference
+=========
+http://jonathanotto.com/blog/backbone_js_for_the_everyman.html
+	- listen to a model change
+		- some_model.set({name: 'new some thing'})
+		- this.model.on("change", function(){ this.render(); })
+
+To do
+=====
+1. Still don't know how to leave trace on the map
+2. 
+
+System Architecture
+===================
+WBD (parent object)
+	- indicators
+		- an array stores all the indiciators
+	- defaultXIndicator
+	- defaultYIndicator
+
+Entries
+	- allData
+	- selDataXYPlot
+	- filter
+		- year
+		- region: []
+		- country: []
+	- xAxisDatasetName
+	- yAxisDatasetName
+	- contentDisplayed = "region" or "country"
+
+	- initialize()
+	- render()
+	- renderAxes()
+
+	- applyFilter()
+		- everytime when someone change the filter, call render()
+		- when the dataset is changed, re-render axes renderAxes()
+	- setRegionFilter()
+	- setCountryFilter()
+
+
+	- setXAxisDatasetName()
+	- setYAxisDatasetName()
+	- getXAxisDatasetName()
+	- getYAxisDatasetName()
+
+XYPlotView
+* Add time selector
+* Add model listener, whenever someone change the model(myEntries), View would respond to that change
+* Process nations.json to get region & population data
+
+MapView
+* select which API to use 
+	- Polymaps, http://polymaps.org/
+	- Colorbrewer, http://colorbrewer2.org/
+	- highcharts, http://www.highcharts.com/
+
+* Members & Methods
+	- initialize()
+	- render()
+
+	Filter related 
+	- toggleSelectedCountry() 
+	- toggleSelectedRegion()
+
+
+* Render non-interactive data first
+	- initialize()
+	- render()
+
+DisplayOptionView
+* use jQuery & Backbone.js
+	- selectXAxisDataset()
+		- change the model myEntries.xAxisDatasetName = ...
+		- change the model myEntries.yAxisDatasetName = ...
+			- (Once the axes dataset change, XYPlotView and MapView would change)
+			- NOTE: in XYPlotView & MapView, we cannot hard-coded axes data, should always get from myEntries model
+
+	- selectYAxisDataset()
+	- setDisplayByContinent()
+	- setDisplayByCountry()
+
+IndValueFilterView
+* use jQuery & Backbone.js
+	- defaultTab
+	- currentTab
+
+	- switchTab()
+		- use jQuery to switch to another div?
+	- render()
+		- render all the tabs, and select the default tab
+	- setFilterByIndValues(String indicator, Array range)
+		- modify the 
+
+RegionFilterView
+* Methods 
+	- initialize()
+	- render()
+	- select()
+	- search(input) or inViewfilter()
+	- toggleFilterbyRegion()
+		- add or remove a region in the filter object
+		- re-render all the views ( XYPlotView, MapView, RegionFilterView )
+		- NOTE: DisplayOptionView and FilterByIndicatorView wont change 
+	- Questions:
+		- when select a region, is it collapsable so more countries would be shown under the region name?
+
+
+
+
+
+
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,19 @@
 
 var WBD = WBD || {};
 
+// Filter Object Prototype
+// an Entries Object contains one filter object
+// NOTE: in order for other views to get or set the attributes in filter object
+//       the best practice is to create another model to stores the filter 
+//       attributes
+WBD.Filter = Backbone.Model.extend({
+  defaults: {
+    year: 2000, // year filter 
+    country: [], // country filter 
+    region: [] // region filter
+  }
+});
+
 // Entries Prototype
 WBD.Entries = Backbone.Model.extend({
 
@@ -9,43 +22,44 @@ WBD.Entries = Backbone.Model.extend({
     
     allData: [], // [{'country': 'taiwan', 'year': '1234' ... }, ...]
     selDataXYPlot: [],
-    
-    filter: {
-      year: 2000, // year filter 
-      country: [], // country filter
-    } 
+    filter: {}
   },
 
   initialize: function(opts){
     this.allData = opts.allData;
-    this.filter = opts.filter;
-
+    this.set("filter", new WBD.Filter());
+    this.set("selDataXYPlot", []);
     this.applyFilter();
+    this.get("filter").bind("change", this.applyFilter, this);
   },
 
   // filter allData to selectedData
   applyFilter: function(){
     var that = this;
-
+    // console.log("applyFilter");
+    // console.log(this.get("selDataXYPlot"));
     /* filter by year 
     * this.selDataXYPlot = [
     *   { country: 'taiwan', 'key1' : '...', 'gni' : '...' },
     *   { ... }  
     * ]
     */
-    this.selDataXYPlot = this.allData.map(function(d){
+
+    var filteredData = that.allData.map(function(d){
+      // d is an attribute in the array
       var key;
       var returnObj = {};
 
       for( key in d){
-        console.log("KEY");
-        console.log(key);
+        // console.log("KEY");
+        // console.log(key);
         if(d.hasOwnProperty(key)){
           // Only check the parts that's not inherited from other places
-          console.log("HI");
+          // console.log("HI");
           if(d[key] instanceof Array){
             // it is an indicator, such as "gni" ... 
-            returnObj[key] = that.interpolateValues(d[key], that.filter.year);
+            returnObj[key] = that.interpolateValues(d[key], that.get("filter").get("year"));
+            
           }
         }
       }
@@ -58,30 +72,39 @@ WBD.Entries = Backbone.Model.extend({
         return returnObj;
       }
     });
+    
+    this.set("selDataXYPlot", filteredData);
 
-    console.log("this getSelDataXYPlot");
-    console.log(this.selDataXYPlot);
   },
 
   getSelDataXYPlot: function(){
-    return this.selDataXYPlot;
+    return this.get("selDataXYPlot");
   },
 
   getAllData: function(){
-    return this.allData;
+    return this.get("allData");
   },
 
   interpolateValues: function(values, year){
-    // Copy from https://github.com/mbostock/bost.ocks.org/blob/gh-pages/mike/nations/index.html
-    var i = d3.bisectLeft(values, year, 0, values.length - 1),
+    var d3_bisector = d3.bisector(function(d){ return d[0];}).left;
+
+    var i = d3_bisector(values, year, 0, values.length - 1),
         a = values[i];
+
     if (i > 0) {
       var b = values[i - 1],
           t = (year - a[0]) / (b[0] - a[0]);
       return a[1] * (1 - t) + b[1] * t;
     }
     return a[1];
-  }
+  },
+
+  // Override the set method to include applyFilter
+  // set: function(attributes, options) {    
+  //   Backbone.Model.prototype.set.call(this, attributes, options);
+  //   this.applyFilter();
+  //   return this;
+  // }
 });
 
 
@@ -93,9 +116,11 @@ WBD.XYPlot = Backbone.View.extend({
   tagName: 'div',
 
   //Note: there is no defaults in View
-  xAxisData: 'gni',
-  yAxisData: 'life_expectency',
+  xAxisDatasetName: 'gni',
+  yAxisDataName: 'life_expectency',
   year: 2000, //temporal use
+  yearSelector: 2000, //default
+  yearLabel: "", // default
   
   // Style Members
   width: 500,
@@ -110,7 +135,11 @@ WBD.XYPlot = Backbone.View.extend({
 
   initialize: function(opts){
     $('#main').append(this.el);
-    
+    this.model = opts.model;
+
+    // Listen to changes on model
+    this.model.bind("change:selDataXYPlot", this.render, this );
+
     // Members
     this.svg = d3.select(this.el).append("svg")
         .attr("width", this.width + this.margins.left + this.margins.right ) // hard-coded right now
@@ -119,81 +148,271 @@ WBD.XYPlot = Backbone.View.extend({
         .attr("transform","translate("+this.margins.left+","+this.margins.top+")");
 
 
-    this.renderAxes();
-    this.render();
+    this.initAxes();
+    this.initYearSelector();
+    
+    this.initChart();
+    
   },
 
-  renderAxes : function() {
+  initAxes : function() {
+    var that = this;
+    
+    // Create x, y scales and x, y axies 
+    this.initScales();
+
+    // Need to change this part in the future
+    // When the dataset is changed, the x-axis need to be removed/updated
+    // Render Axes 
+    this.xAxisEl = this.svg.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", "translate(0,"+that.height+")") 
+      .call(that.xAxis);
+
+    this.yAxisEl = this.svg.append("g")
+      .attr("class", "y-axis")
+      .call(that.yAxis);
+
+    // Add an x-axis label.
+    this.xAxisLabelEl = this.svg.append("text")
+        .attr("class", "x label")
+        .attr("text-anchor", "end")
+        .attr("x", that.width)
+        .attr("y", that.height - 6)
+        .text(that.xAxisDatasetName);
+
+    // Add a y-axis label.
+    this.yAxisLabelEl = this.svg.append("text")
+        .attr("class", "y label")
+        .attr("text-anchor", "end")
+        .attr("y", 6)
+        .attr("dy", ".75em")
+        .attr("transform", "rotate(-90)")
+        .text(that.yAxisDataName);
+  },
+
+  updateAxes: function(){
+    var that = this;
+
+    // Re-render x, y axes 
+    this.xAxisEl
+      .call(that.xAxis);
+
+    this.yAxisEl
+      .call(that.yAxis);
+
+    // Update the x, y axis label.
+    this.xAxisLabelEl
+      .text(that.xAxisDatasetName);
+
+    this.yAxisLabelEl
+      .text(that.yAxisDataName);
+
+  },
+
+  initScales: function(){
     var that = this;
     // Create Scales 
     this.xScale = d3.scale.linear()
-      .domain( [ 0, d3.max(this.model.getSelDataXYPlot(), function(d){
-        return d[that.xAxisData]; 
+      .domain( [ 0, d3.max(that.model.getSelDataXYPlot(), function(d){
+        return d[that.xAxisDatasetName]; 
       })] 
       )
       .range([0, this.width]);
 
     this.yScale = d3.scale.linear()
-      .domain( [ 0, d3.max(this.model.getSelDataXYPlot(), function(d){
-        return d[that.yAxisData]; 
+      .domain( [ 0, d3.max(that.model.getSelDataXYPlot(), function(d){
+        return d[that.yAxisDataName]; 
       }) ] 
       )
-      .range([this.height, 0]);
+      .range([that.height, 0]);
 
 
     // Create Axes
     this.xAxis = d3.svg.axis()
-      .scale(this.xScale)
+      .scale(that.xScale)
       .orient("bottom");
 
     this.yAxis = d3.svg.axis()
-      .scale(this.yScale)
+      .scale(that.yScale)
       .orient("left");
+  },
 
-    // Render Axes 
-    this.svg.append("g")
-      .attr("class", "x-axis")
-      .attr("transform", "translate(0,"+this.height+")") 
-      .call(this.xAxis);
+  initYearSelector: function(){
+      // Add the year label; the value is set on transition.
+      var that = this;
+      this.yearLabel = this.svg.append("text")
+        .attr("class", "year label")
+        .attr("text-anchor", "end")
+        .attr("y", that.height - 24)
+        .attr("x", that.width)
+        .text(that.model.get("filter").get("year"));
 
-    this.svg.append("g")
-      .attr("class", "y-axis")
-      .call(this.yAxis);
+      // Add on overlay for the year label
+      this.yearSelectorBox = this.yearLabel.node().getBBox();
+      // console.log("this.yearSelectorBox");
+      // console.log(this.yearSelectorBox);
+
+      this.yearSelector = this.svg.append("rect")
+        .attr("class", "year-selector overlay")
+        .attr("x", that.yearSelectorBox.x)
+        .attr("y", that.yearSelectorBox.y)
+        .attr("width", that.yearSelectorBox.width)
+        .attr("height", that.yearSelectorBox.height)
+        .data([that]) // this is the only way to pass the model into function
+                      // so inside a callback, the context could remain unchanged
+        .on("mouseover", that.enableYearSelector);
+
+  },
+
+  updateYearSelector: function(){
+      var that = this;
+      this.yearLabel
+        .text(that.model.get("filter").get("year"));
   },
 
   render: function() {
-    
+    var that = this;
     console.log("Render XYPlot ... ");
-    console.log(this.model.getSelDataXYPlot());
+    // console.log(this.model.getSelDataXYPlot());
     
     // var allData = d3.nest()
     //   .key(function(d){ return d.country})
     //   .key(function(d){ return d.year})
     //   .entries(this.model.get("selDataXYPlot"));
+    
+
+
+
+    // Add a dot per nation
+    // this.baseGraph is a selection ?
+    this.baseGraph
+        // d.country is the key to identify different array element
+        .data(this.model.getSelDataXYPlot(), function(d){ return d.country})
+      .attr("cx", function(d){ 
+        // It is possible that some countries won't have this indicator record
+        // therefore, return 0 
+
+        if(d.country == "Canada"){
+          console.log(d.country);
+          console.log(d.gni);
+          console.log(that.xScale(d[that.xAxisDatasetName] || 0 ));
+        }
+        return that.xScale(d[that.xAxisDatasetName] || 0 ); 
+      })
+      .attr("cy", function(d){ 
+        // It is possible that some countries won't have this indicator record
+        // therefore, return 0 
+        return that.yScale(d[that.yAxisDataName] || 0 ); 
+      })
+      .attr("r", function(d){
+        return that.dotSize;
+      });
+    
+
+    // console.log("exit()");
+    // console.log(circles.exit());
+    // this.baseGraph.exit().remove();
+    this.updateYearSelector();
+    console.log("done");
+  },
+
+  initChart: function(){
     var that = this;
-
+    // Add a dot per nation
     this.baseGraph = this.svg.append("g")
-      .attr("class", "base-graph")
-
-
-    this.baseGraph.selectAll("g")
+        .attr("class", "dots")
+      .selectAll("dot")
         .data(this.model.getSelDataXYPlot())
       .enter().append("circle")
-        .attr("class", "country")
+        .attr("class", "dot")
         .attr("id", function(d){ return d.country; })
         .attr("cx", function(d){ 
           // It is possible that some countries won't have this indicator record
           // therefore, return 0 
-          return that.xScale(d[that.xAxisData] || 0 ); 
+
+          if(d.country == "Canada"){
+            console.log(d.country);
+            console.log(d.gni);
+            console.log(that.xScale(d[that.xAxisDatasetName] || 0 ));
+          }
+          return that.xScale(d[that.xAxisDatasetName] || 0 ); 
         })
         .attr("cy", function(d){ 
           // It is possible that some countries won't have this indicator record
           // therefore, return 0 
-          return that.yScale(d[that.yAxisData] || 0 ); 
+          return that.yScale(d[that.yAxisDataName] || 0 ); 
         })
         .attr("r", function(d){
           return that.dotSize;
         });
+
+    
+
+    // console.log("exit()");
+    // console.log(circles.exit());
+    // this.baseGraph.exit().remove();
+    this.updateYearSelector();
+  },
+
+  enableYearSelector: function(d){
+    // this = the current DOM element, not the Model 
+
+    var self = d;
+    var that = self;
+
+    self.yearScale = d3.scale.linear()
+            .domain([1990, 2010]) // hard-coded
+            .range([that.yearSelectorBox.x + 10, that.yearSelectorBox.x + that.yearSelectorBox.width - 10])
+            .clamp(true);
+
+    // Cancel the current transition, if any.
+    self.svg.transition().duration(0);
+
+    self.yearSelector
+        .on("mouseover", mouseover)
+        .on("mouseout", mouseout)
+        .on("mousemove", mousemove)
+        .on("touchmove", mousemove);
+
+    function mouseover() {
+      that.yearLabel.classed("active", true);
+    }
+
+    function mouseout() {
+      that.yearLabel.classed("active", false);
+    }
+
+    function mousemove() {
+      that.setFilterByYear(that.yearScale.invert(d3.mouse(this)[0])); // get mouseX
+    }
+  },
+
+  setFilterByYear: function(year){
+    var that = this;
+    that.model.get("filter").set({ year: Math.round(year)}); 
+    // that.yearLabel = Math.round(year);
+  }
+
+});
+
+
+WBD.Map = Backbone.View.extend({
+  className: 'map',
+  id: 'map',
+  tagName: 'div',
+
+  initialize: function(opts){
+    $('#main').append(this.el);
+    this.model = opts.model;
+
+    // Listen to changes on model
+    this.model.bind("change:selDataXYPlot", this.render, this );
+
+  },
+
+  render: function(){
+
   }
 
 });
@@ -211,14 +430,23 @@ WBD.prepareData = function(data){
     .key(function(d){ return d.country; })
     .entries(data);
 
-  console.log("nest");
-  console.log(d);
+  // console.log("nest");
+  // console.log(d);
 
   return data
 }
 
-WBD.interpolateValues = function(){
+// Add a remove method to Array
+// reference: http://javascriptweblog.wordpress.com/
+Array.prototype.remove = function(member) {
 
+  var index = this.indexOf(member);
+
+  if (index > -1) {
+    this.splice(index, 1);
+  }
+
+  return this;
 }
 
 // main function 
@@ -234,15 +462,11 @@ $(document).ready(function(){
       console.log("Read and store data ... ");
       // read data into WBD.Record
       myEntries = new WBD.Entries({
-        allData: WBD.prepareData(data),
-        filter: {
-          year: 2000,
-          country: []
-        }
+        allData: WBD.prepareData(data)
       });
 
-      console.log("Check myEntries");
-      console.log(myEntries.get('allData'));
+      // console.log("Check myEntries");
+      // console.log(myEntries.get('allData'));
       
 
       // Create a XY Plot
@@ -251,8 +475,9 @@ $(document).ready(function(){
         model: myEntries
       });
 
+      myMapView = new WBD.Map({
+        model: myEntries
+      });
+
   });
 });
-
-
-
